7ba33d1451fe281ee12840b404a7b21de76f3a4f
pmnoos <petermagner3@gmail.com>
2025-06-29 13:44:00 +1000

    Restore personal story template and context processor

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,14 @@
 from django.conf import settings
 
+def template_settings(request):
+    """Add template settings context to all templates"""
+    return {
+        'SITE_TITLE': 'My Life Story',
+        'SITE_SUBTITLE': 'My Personal Journey',
+        'SITE_DESCRIPTION': 'A digital memoir of my life experiences, memories, and stories.',
+        'AUTHOR_SIGNATURE': 'Written with love and memories',
+    }
+
 def translations(request):
     """Add translation context to all templates"""
     # Simple fallback translations
