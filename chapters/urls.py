602c1ba19995a9ebd3a3cb54fb279d57d0eb11eb
pmnoos <petermagner3@gmail.com>
2025-06-30 17:01:18 +1000

    Clean up duplicate URLs and move images to static

--- chapters/urls.py
+++ chapters/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 from .views import (
     profile, upload_image, new_chapter, chapter_list, 
@@ -33,6 +31,3 @@ urlpatterns = [
 ]
 
 # Serve media files in development mode
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
