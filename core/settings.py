9359ee067faa32be8a25289692b74542b1e542d8
pmnoos <petermagner3@gmail.com>
2025-07-01 14:21:20 +1000

    Add WhiteNoise back for Render deployment with clean image setup

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-r^c!!%=!yh&f7#4pdimu&e)v_kfc-8u201xl9rn=ay@a2+-#lc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = ['*']  # For development and testing on Render
 
 # Application definition
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-   # "whitenoise.middleware.WhiteNoiseMiddleware",  # Move this to position 2
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Move this to position 2
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -95,6 +95,9 @@ LOCALE_PATHS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# WhiteNoise configuration for Render
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Media files - CLEAN and SIMPLE  
 MEDIA_URL = '/media/'
